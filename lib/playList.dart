b402515d1142eb02d1157605b41b2ffdba15fa47
LindemanCode <wuliyounuo@126.com>
2025-03-25 15:38:24 +0800

    直接导入歌曲到播放列表,同时更新到全部音乐-梁立达-2025.03.25

--- lib/playList.dart
+++ lib/playList.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'utils/index.dart';
 import 'components/songList.dart';
 import 'home.dart';
 import 'components/bottomPlay.dart';
+import 'dart:io';
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class MPlayList extends StatefulWidget {
   final playListName;
@@ -16,6 +22,9 @@ class MPlayList extends StatefulWidget {
 class MPlayListState extends State<MPlayList> {
   final playListName;
   MPlayListState({Key? key, @required this.playListName});
+
+  var uuid = Uuid();
+  late Key _sListkey = Key(uuid.v1());
 
   @override
   void initState() {
@@ -46,6 +55,53 @@ class MPlayListState extends State<MPlayList> {
         title: const Text('歌单'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.download_sharp, color: Colors.white),
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true);
+                final Directory? directory = await getApplicationDocumentsDirectory();
+                if (result != null) {
+                  dynamic tempArr = [];
+                  for (var i = 0; i < result.files.length; i++) {
+                    var path = result.files[i].path;
+                    dynamic metadata = await MetadataRetriever.fromFile(File(path!));
+                    var name = result.files[i].name;
+                    var extension = result.files[i].extension;
+                    var alias = metadata.trackName ?? name.split('.$extension')[0];
+                    var id = uuid.v1();
+                    var logoPath = '';
+                    if (metadata.albumArt != null && directory != null) {
+                      // 保存图片到应用文件目录
+                      logoPath = '${directory.path}/$alias$id';
+                      await File(logoPath).writeAsBytes(metadata.albumArt);
+                    }
+                    tempArr.insert(0, {
+                      'id': id,
+                      'name': name,
+                      'alias': alias,
+                      'logo': logoPath,
+                      'parentId': [],
+                      'path': path,
+                      'artist': metadata.trackArtistNames == null ? '-' : metadata.trackArtistNames[0],
+                      'album': metadata.albumName ?? '',
+                      'isChecked': false,
+                    });
+                  }
+                  tempArr.sort((a, b) => a['name'].toString().compareTo(b['name'].toString()));
+                  dynamic allSongList = await getLocalList('allMusic');
+                  allSongList.insertAll(0, tempArr);
+                  setLocalList('allMusic', allSongList);
+                  for (int sidx = tempArr.length - 1; sidx > -1; sidx--) {
+                    await updateSong(tempArr[sidx], 'add', 'playList$playListName');
+                  }
+                  setState(() {
+                    _sListkey = Key(uuid.v1());
+                  });
+                }
+              }
+          ),
+        ],
       ),
       body: SafeArea(
         child: Container(
@@ -63,7 +119,7 @@ class MPlayListState extends State<MPlayList> {
                       color: Color(0xFFFFFFFF),
                       borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15))
                   ),
-                  child: SongList(listName: 'playList$playListName'),
+                  child: SongList(key: _sListkey, listName: 'playList$playListName'),
                 ),
               ),
             ],
